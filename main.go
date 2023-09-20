15c2dc3f35dcef3fa7f0b3972bcbf6f456349350
aagrawa0913 <143794448+aagrawa0913@users.noreply.github.com>
2023-09-20 15:59:20 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,5 +2,32 @@
 package main
 
 func main() {
-  println("Ba dum, tss!")
+  println("Ba dum, tss!")
+}
+
+import (
+    "fmt"
+    "os"
+)
+
+func main() {
+    fmt.Println("SARIF test")
+
+    secretKey := os.Getenv("SECRET_KEY")
+    fmt.Println("Secret Key:", secretKey)
+
+    password := "supersecret"
+    fmt.Println("Password:", password)
+
+    userInput := "userInputValue"
+    query := "SELECT * FROM users WHERE username='" + userInput + "'"
+    fmt.Println("SQL Query:", query)
+
+    // encryptedData := weakEncrypt("sensitive data")
+    // fmt.Println("Encrypted Data:", encryptedData)
+
+    username := "admin"
+    pwd := "f62e5bcda4fae4f82370da0c6f20697b8f8447ef"
+
+    fmt.Println("Doing something with: ", username, pwd)
 }
